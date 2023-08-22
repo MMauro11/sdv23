f58815f71b198217afe23a540ad37231d1f0bf68
Marco M <93marco.mauro@gmail.com>
2023-08-22 20:58:34 +0200

    FEATURE Menu - (toDo Fix Volume)

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    /*public AudioMixer Mixer;
+    public Slider VolumeSlider;
+*/
+    void Start() {
+/*
+        if (PlayerPrefs.HasKey("VolumePrefs")) {
+
+            float volumePrefs = PlayerPrefs.GetFloat("VolumePrefs");
+            Mixer.SetFloat("MusicVol", Mathf.Log10(volumePrefs) * 20);
+            VolumeSlider.value = volumePrefs;
+        } else 
+        {
+            float initialVolume = VolumeSlider.value;
+            Mixer.SetFloat("MusicVol", Mathf.Log10(initialVolume) * 20);
+        }
+*/
+
+    }
+
+    public void PlayGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame() {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
